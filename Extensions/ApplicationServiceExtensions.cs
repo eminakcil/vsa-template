7999d92d77899e15f9a8ecf2c8fc118b301cd37b
eminakcil <eminakcil@outlook.com>
2026-02-19 10:27:43 +0300

    feat: integrate MediatR and FluentValidation with Pipeline Behavior

--- Extensions/ApplicationServiceExtensions.cs
+++ Extensions/ApplicationServiceExtensions.cs
@@ -1,9 +1,21 @@
+using FluentValidation;
+using VsaTemplate.Common.Behaviors;
+
 namespace VsaTemplate.Extensions;
 
 public static class ApplicationServiceExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        var assembly = typeof(Program).Assembly;
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(assembly);
+
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+
+        services.AddValidatorsFromAssembly(assembly);
         return services;
     }
 }
